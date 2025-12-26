5605328e068ca7435989ea557233e9946729022d
Márcio <marcionitao@gmail.com>
2025-12-26 14:04:16 +0100

    add initial database setup with SQLAlchemy and session management

---
+++ lieb_car_api/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'sqlite:///cars.db'
+
+engine = create_engine(DATABASE_URL)
+
+# cria uma sessäo na DB
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# função do SQLAlchemy que cria uma classe base especial.
+Base = declarative_base()
+
+def get_session():
+  session = SessionLocal() # cria a sessäo
+  try:
+    yield session
+  finally:
+    session.close()
