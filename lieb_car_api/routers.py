e458f4aa41c800fac89be0006f556755e8389f90
Márcio <marcionitao@gmail.com>
2026-01-04 14:31:05 +0100

    feat: add list cars endpoint and update CarPublic schema with optional fields

--- lieb_car_api/routers.py
+++ lieb_car_api/routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from lieb_car_api.database import get_session
 from lieb_car_api.models import Car
-from lieb_car_api.schemas import CarPublic, CarSchema
+from lieb_car_api.schemas import CarList, CarPublic, CarSchema
 
 router = APIRouter(
     prefix='/api/v1/cars',
@@ -14,15 +15,25 @@ router = APIRouter(
 @router.post(
     '/', response_model=CarPublic, status_code=status.HTTP_201_CREATED
 )
-# abrir o banco de dados, abrir uma conexão com db, salvar o registo na DB e fechar a conexão
 def create_car(
     car: CarSchema,
     session: Session = Depends(get_session),  # noqa: B008
 ):
-    car = Car(
-        **car.model_dump()
-    )  # Converte o objeto Pydantic (CarSchema) em um objeto SQLAlchemy (Car)
-    session.add(car)  # adiciona os dados
-    session.commit()  # salva os dados
-    session.refresh(car)  # atualiza os dados
+    # Converte o objeto Pydantic(CarSchema) em um objeto SQLAlchemy(Car) que a DB entende
+    car = Car(**car.model_dump())
+    session.add(car)
+    session.commit()
+    session.refresh(car)
     return car
+
+
+@router.get(path='/', response_model=CarList, status_code=status.HTTP_200_OK)
+def list_cars(
+    session: Session = Depends(get_session),  # noqa: B008
+    offset: int = 0,  # a partir zero
+    limit: int = 100,  # traga 100 resultados
+):
+    # session.scalars busca na Table Car usando paginação
+    query = session.scalars(select(Car).offset(offset).limmit(limit))
+    cars = query.all()  # busque todos os carros
+    return {'cars': cars}
--- lieb_car_api/schemas.py
+++ lieb_car_api/schemas.py
@@ -18,7 +18,12 @@ class CarPublic(CarSchema):
     id: int
     brand: str
     model: str
-    color: str
-    factory_year: int
-    model_year: int
-    description: str
+    color: Optional[str] = None
+    factory_year: Optional[int] = None
+    model_year: Optional[int] = None
+    description: Optional[str] = None
+
+
+# achma que lista os dados
+class CarList(BaseModel):
+    cars: list[CarPublic]
